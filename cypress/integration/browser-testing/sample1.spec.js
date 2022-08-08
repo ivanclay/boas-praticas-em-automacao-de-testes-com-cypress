53f1d555dd5bf94f4daeaa27064bca3c8d068b01
ivanclay <ivan.clay.moura@gmail.com>
2022-08-08 09:05:44 -0300

    feat: Browser testing or App feature testing?
    It shows and fix bad pratices to test app feature

--- cypress/integration/browser-testing/sample1.spec.js
+++ cypress/integration/browser-testing/sample1.spec.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+
 describe('Browser testing bad practice - Sample 1', () => {
   beforeEach(() => {
     cy.visit('https://notes-serverless-app.com')
@@ -7,5 +9,17 @@ describe('Browser testing bad practice - Sample 1', () => {
     cy.contains('.nav a', 'Login').click()
 
     cy.url().should('be.equal', 'https://notes-serverless-app.com/login')
+  })
+})
+
+describe('Browser testing GOOD practice - Sample 1', () => {
+  beforeEach(() => {
+    cy.visit('https://notes-serverless-app.com')
+  })
+
+  it('directs the user to the login page when clicking the login link', () => {
+    cy.contains('.nav a', 'Login')
+      .should('have.attr', 'href', '/login')
+      .should('not.have.attr', 'target')
   })
 })
--- cypress/integration/browser-testing/sample2.spec.js
+++ cypress/integration/browser-testing/sample2.spec.js
@@ -1,3 +1,6 @@
+/// <reference types="Cypress" />
+
+
 describe('Browser testing bad practice', () => {
   beforeEach(() => {
     cy.visit('https://hackernews-seven.vercel.app')
@@ -7,6 +10,43 @@ describe('Browser testing bad practice', () => {
     cy.get('.table-row a')
       .first()
       .click()
+
+    // Assert that a new tab was opened in the correct URL
+  })
+})
+
+describe('Browser testing GOOD practice', () => {
+  beforeEach(() => {
+    cy.visit('https://hackernews-seven.vercel.app')
+  })
+
+  it('tests a app feature', () => {
+    cy.get('.table-row a')
+      .first()
+      .should('have.attr', 'target', '_blank')
+
+    // Assert that a new tab was opened in the correct URL
+  })
+})
+
+describe('Browser testing BETTER practice', () => {
+  beforeEach(() => {
+
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories' }
+    ).as('getStories')
+
+    cy.visit('https://hackernews-seven.vercel.app')
+    cy.wait('@getStories')
+  })
+
+  it('tests a app feature', () => {
+    cy.get('.table-row a')
+      .first()
+      .should('have.attr', 'href', 'https://example.com/lc-jg')
+      .and('have.attr', 'target', '_blank')
 
     // Assert that a new tab was opened in the correct URL
   })
